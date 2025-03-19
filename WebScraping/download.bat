012a0833d9c19e573f855b01d2ca4fe8521a6490
tanmay.pyspark <tanmaym.mandal@gmail.com>
2025-03-19 23:59:42 +0530

    author class change

--- WebScraping/download.bat
+++ WebScraping/download.bat
@@ -1,3 +1,3 @@
 @echo off
-python "C:\Users\tanma\Desktop\GIT_PUSH\Python_Project\WebScraping\Wrapper_WebScrap.py"
+python -B "C:\Users\tanma\Desktop\GIT_PUSH\Python_Project\WebScraping\Wrapper_WebScrap.py"
 pause
